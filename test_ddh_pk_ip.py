1df9e651d3b42b994da798320e2afe818f66e1d9
Cecylia Borek <cecylia.borek@gmail.com>
2022-02-23 13:14:52 +0200

    updates test vectors to be shorter

--- test_ddh_pk_ip.py
+++ test_ddh_pk_ip.py
@@ -5,11 +5,12 @@ from ddh_pk_ip import DDH_PK
 
 def testFinResult():
     fe = DDH_PK()
-    mpk, msk = fe.setUp(8, 10)
 
+    x = [1, 4, 6, 3, 5]
+    y = [1, 2, 1, 2, 1]
+    n = len(x)
 
-    x = [1, 4, 6, 3, 5, 8, 9, 0, 3, 4]
-    y = [1, 2, 1, 2, 1, 2, 1, 2, 1, 2]
+    mpk, msk = fe.setUp(8, n)
     ciphertext = fe.encrypt(mpk, x)
 
     func_key = fe.getFunctionalKey(msk, y)
@@ -24,11 +25,5 @@ def testFinResult():
 
 
 if __name__=="__main__":
-    x = [1, 4, 6, 3, 5, 8, 9, 0, 3, 4]
-    y = [1, 2, 1, 2, 1, 2, 1, 2, 1, 2]
-
-    inner_prod = np.inner(x, y)
-
-    print(inner_prod)
     
     testFinResult()
