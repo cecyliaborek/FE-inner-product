42644e65828b4c5401c67c75f505e47941766489
Cecylia Borek <cecylia.borek@gmail.com>
2022-03-04 14:30:23 +0200

    modifies tests

---
+++ test_helpers.py
@@ -0,0 +1,25 @@
+from charm.toolbox.integergroup import integer, IntegerGroup
+
+from helpers import getInt, getModulus
+import unittest
+
+class TestHelpers(unittest.TestCase):
+
+    def testGetInt(self):
+        p = integer(123456789)
+        print(p)
+        i = getInt(p)
+
+        self.assertEqual(123456789, i)
+
+    def testGetMod(self):
+        group = IntegerGroup()
+        group.paramgen(1024)
+        g = group.randomGen()
+
+        mod = getModulus(g)
+
+
+
+if __name__=="__main__":
+    unittest.main()
