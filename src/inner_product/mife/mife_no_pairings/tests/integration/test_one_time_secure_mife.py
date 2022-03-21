1ff36160db6dca9301af73f464052971f0ba3a3b
Cecylia Borek <cecylia.borek@gmail.com>
2022-03-21 14:50:07 +0200

    fin result test added and passed

--- src/inner_product/mife/mife_no_pairings/tests/integration/test_one_time_secure_mife.py
+++ src/inner_product/mife/mife_no_pairings/tests/integration/test_one_time_secure_mife.py
@@ -1,5 +1,6 @@
 import unittest
 
+from src.helpers.helpers import inner_product
 from src.inner_product.mife.mife_no_pairings.function_families import MultiInputInnerProductZl
 from src.inner_product.mife.mife_no_pairings.one_time_secure_mife import OneTimeSecureMIFE
 
@@ -12,14 +13,16 @@ class TestOneTimeSecureMIFE(unittest.TestCase):
 
     def test_final_result(self):
         key = self.mife.set_up(2)
-        x = [[10, 20], [30, 40], [50, 60], [70, 80]]
+        x = [[10, 20], [30, 4], [50, 60], [70, 80]]
         y = [[1, 2], [3, 4], [5, 6], [7, 8]]
         ciphertext = []
         for i in range(len(x)):
             ciphertext.append(self.mife.encrypt(key, i, x[i]))
         func_key = self.mife.get_functional_key(key, y)
         result_ip = self.mife.decrypt(func_key, ciphertext, y)
-        print(result_ip)
+        expected_ip = sum([inner_product(x[i], y[i]) for i in range(len(x))]) % self.mife.modulus
+        self.assertEqual(expected_ip, result_ip)
+        print(expected_ip, result_ip)
 
 
 if __name__ == '__main__':
