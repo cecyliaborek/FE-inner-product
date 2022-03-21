e64453a4a2acae16c180e8dad3bfca0884aa6f64
Cecylia Borek <cecylia.borek@gmail.com>
2022-03-21 12:07:25 +0200

    support for vector of vectors, some setup functionality moved to init

--- src/inner_product/mife/mife_no_pairings/one_time_secure_mife.py
+++ src/inner_product/mife/mife_no_pairings/one_time_secure_mife.py
@@ -1,49 +1,53 @@
 from src.errors.vector_size_mismatch_error import VectorSizeMismatchError
 from src.errors.wrong_vector_for_provided_key import WrongVectorForProvidedKey
-from src.helpers.helpers import inner_product_modulo, inner_product, get_random_from_Zl, add_vectors_mod
+from src.helpers.helpers import inner_product, get_random_from_Zl, add_vectors_mod
 from src.inner_product.mife.mife_no_pairings.function_families import MultiInputInnerProductZl
 from typing import List
 
 
 class OneTimeSecureMIFE:
 
-    def __init__(self):
-        self.vector_len = None
-        self.modulus = None
-        self.inner_vector_len = None
-
-    def set_up(self, security_param: int, func_descr: MultiInputInnerProductZl) -> List[List[int]]:
+    def __init__(self, func_descr: MultiInputInnerProductZl):
         self.vector_len = func_descr.n
         self.inner_vector_len = func_descr.m
         self.modulus = func_descr.L
-        u = [0] * self.vector_len
+
+    def set_up(self, security_param: int) -> List[List[int]]:
+        key = [0] * self.vector_len
         for i in range(self.vector_len):
-            u[i] = [get_random_from_Zl(self.modulus) for _ in range(self.inner_vector_len)]
-        return u
+            key[i] = [get_random_from_Zl(self.modulus) for _ in range(self.inner_vector_len)]
+        return key
 
-    def encrypt(self, u, i, x_i):
-        if i > len(u):
-            raise WrongVectorForProvidedKey(f'Index {i} out of range for key {u}')
+    def encrypt(self, key: List[List[int]], i: int, x_i: List[int]) -> List[int]:
+        if i > len(key):
+            raise WrongVectorForProvidedKey(f'Index {i} out of range for key {key}')
         try:
-            return add_vectors_mod(u[i], x_i, self.modulus)
+            return add_vectors_mod(key[i], x_i, self.modulus)
         except VectorSizeMismatchError:
-            if len(x_i) < len(u):
-                raise WrongVectorForProvidedKey(f'Vector {x_i} too short provided key')
-            raise WrongVectorForProvidedKey(f'Vector {x_i} too long for provided key')
+            raise WrongVectorForProvidedKey(f'Vector {x_i} too long for dimension {i} of the provided key')
 
-    def get_functional_key(self, u: List[int], y: List[int]):
-        # todo: update to vector of vectors support
-        try:
-            return inner_product_modulo(u, y, self.modulus)
-        except VectorSizeMismatchError:
-            if len(y) < len(u):
-                raise WrongVectorForProvidedKey(f'Vector {y} too short provided key')
-            raise WrongVectorForProvidedKey(f'Vector {y} too long for provided key')
+    def get_functional_key(self, key: List[List[int]], y: List[List[int]]) -> int:
+        if len(key) != len(y):
+            raise WrongVectorForProvidedKey('Different lengths of the provided key and vector')
+        n = len(key)
+        intermediate = [None] * n
+        for i in range(n):
+            try:
+                intermediate[i] = inner_product(key[i], y[i])
+            except VectorSizeMismatchError:
+                raise WrongVectorForProvidedKey(f'Different lengths of dimension {i} \
+                for the provided key and the vector')
+        return sum(intermediate) % self.modulus
 
-    def decrypt(self, func_key, ciphertext, y):
-        try:
-            return (inner_product(ciphertext, y) - func_key) % self.modulus
-        except VectorSizeMismatchError:
-            if len(y) < len(ciphertext):
-                raise WrongVectorForProvidedKey(f'Vector {y} too short provided ciphertext')
-            raise WrongVectorForProvidedKey(f'Vector {y} too long for provided ciphertext')
+    def decrypt(self, func_key: int, ciphertext: List[List[int]], y: List[List[int]]) -> int:
+        if len(ciphertext) != len(y):
+            raise WrongVectorForProvidedKey(f'Different lengths of provided ciphertext and vector')
+        n = len(ciphertext)
+        intermediate = [None] * n
+        for i in range(n):
+            try:
+                intermediate[i] = inner_product(ciphertext[i], y[i])
+            except VectorSizeMismatchError:
+                raise WrongVectorForProvidedKey(f'Different lengths of dimension {i} \
+                for the provided ciphertext and the vector')
+        return (sum(intermediate) - func_key) % self.modulus
