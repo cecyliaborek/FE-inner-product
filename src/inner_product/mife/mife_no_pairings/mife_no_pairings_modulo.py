c69ba88543c5b6b99434011b6654593dd4f64064
Cecylia Borek <cecylia.borek@gmail.com>
2022-04-19 16:35:24 +0200

    add separate file for mife no pairings modulo

---
+++ src/inner_product/mife/mife_no_pairings/mife_no_pairings_modulo.py
@@ -0,0 +1,17 @@
+"""
+Abdalla, Michel et al. Multi-input functional encryption schemes without pairings for inner product over integers
+    modulo L
+
+
+| From:         Abdalla, Michel et al. “Multi-Input Functional Encryption for Inner Products: Function-Hiding
+                Realizations and Constructions Without Pairings.”
+| Published in: Advances in Cryptology – CRYPTO 2018. Cham: Springer International Publishing, 2018. 597–627. Web.
+| DOI:          10.1007/978-3-319-96884-1_20
+
+* type:         private key multi-input functional encryption
+* setting:
+* assumption:
+
+:Authors:       Cecylia Borek
+:Date:          03/2022
+"""
