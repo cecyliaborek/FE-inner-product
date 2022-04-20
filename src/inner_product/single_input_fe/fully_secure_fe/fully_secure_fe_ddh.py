20b8434a9fc2208a854aff3b344b862ed6c9c940
Cecylia Borek <cecylia.borek@gmail.com>
2022-04-20 13:12:22 +0200

    refactor fully secure ddh and lwe short int

--- src/inner_product/single_input_fe/fully_secure_fe/fully_secure_fe_ddh.py
+++ src/inner_product/single_input_fe/fully_secure_fe/fully_secure_fe_ddh.py
@@ -16,61 +16,51 @@ Agrawal et al. Fully secure functional encryption scheme from standard assumptio
 :Date:          03/2022
 """
 from src.helpers.helpers import generate_group, get_random_generator, inner_product_group_vector, dummy_discrete_log, \
-    get_int, get_modulus
+    get_int, get_modulus, reduce_vector_mod
 
 from typing import List
 import numpy as np
 
 
-class MasterPublicKey:
+class FullySecureFE:
 
-    def __init__(self, group, gen1, gen2, h):
+    def __init__(self, group=None, gen1=None, gen2=None, p=None):
         self.group = group
         self.gen1 = gen1
         self.gen2 = gen2
-        self.h = h
+        self.p = p
 
+    def get_public_params(self):
+        return {'group': self.group, 'gen1': self.gen1, 'gen2': self.gen2, 'p': self.p}
 
-class MasterSecretKey:
-
-    def __init__(self, s, t):
-        self.s = s
-        self.t = t
-
-
-class FullySecureFE:
-
-    @staticmethod
-    def set_up_keys(security_param: int, vector_length: int) -> (MasterPublicKey, MasterSecretKey):
-        group = generate_group(security_param)
-        g = get_random_generator(group)
-        f = get_random_generator(group)
-        s = [group.random() for _ in range(vector_length)]
-        t = [group.random() for _ in range(vector_length)]
-        h = [(g ** s[i]) * (f ** t[i]) for i in range(vector_length)]
-        mpk, msk = MasterPublicKey(group, g, f, h), MasterSecretKey(s, t)
+    def set_up(self, security_param: int, vector_length: int) -> (dict, dict):
+        self.group = generate_group(security_param)
+        self.gen1 = get_random_generator(self.group)
+        self.gen2 = get_random_generator(self.group)
+        self.p = get_modulus(self.gen1)
+        s = [self.group.random() for _ in range(vector_length)]
+        t = [self.group.random() for _ in range(vector_length)]
+        h = [(self.gen1 ** s[i]) * (self.gen2 ** t[i]) for i in range(vector_length)]
+        mpk, msk = {'h': h}, {'s': s, 't': t}
         return mpk, msk
 
-    @staticmethod
-    def get_functional_key(msk: MasterSecretKey, y: List[int]) -> dict:
-        func_key = {'s_y': inner_product_group_vector(msk.s, y), 't_y': inner_product_group_vector(msk.t, y)}
+    def get_functional_key(self, msk: dict, y: List[int]) -> dict:
+        y = reduce_vector_mod(y, self.p)
+        func_key = {'s_y': inner_product_group_vector(msk['s'], y), 't_y': inner_product_group_vector(msk['t'], y)}
         return func_key
 
-    @staticmethod
-    def encrypt(mpk: MasterPublicKey, x: List[int]) -> dict:
+    def encrypt(self, mpk: dict, x: List[int]) -> dict:
         ciphertext = {}
-        r = mpk.group.random()
-        ciphertext['c'] = mpk.gen1 ** r
-        ciphertext['d'] = mpk.gen2 ** r
-        ciphertext['e'] = [(mpk.gen1 ** x[i]) * (mpk.h[i] ** r) for i in range(len(x))]
+        x = reduce_vector_mod(x, self.p)
+        r = self.group.random()
+        ciphertext['c'] = self.gen1 ** r
+        ciphertext['d'] = self.gen2 ** r
+        ciphertext['e'] = [(self.gen1 ** x[i]) * (mpk['h'][i] ** r) for i in range(len(x))]
         return ciphertext
 
-    @staticmethod
-    def decrypt(mpk: MasterPublicKey, func_key: dict, ciphertext: dict, y: List[int], limit: int) -> int:
+    def decrypt(self, func_key: dict, ciphertext: dict, y: List[int], limit: int) -> int:
         e = ciphertext['e']
-        intermediate = np.prod([e[i] ** y[i] for i in range(len(y))])/(
-            ciphertext['c'] ** func_key['s_y'] * ciphertext['d'] ** func_key['t_y']
+        intermediate = np.prod([e[i] ** y[i] for i in range(len(y))]) / (
+                ciphertext['c'] ** func_key['s_y'] * ciphertext['d'] ** func_key['t_y']
         )
-        return dummy_discrete_log(get_int(mpk.gen1), get_int(intermediate), get_modulus(mpk.gen1), limit)
-
-
+        return dummy_discrete_log(get_int(self.gen1), get_int(intermediate), get_modulus(self.gen1), limit)
--- src/inner_product/single_input_fe/fully_secure_fe/fully_secure_fe_lwe_short_int.py
+++ src/inner_product/single_input_fe/fully_secure_fe/fully_secure_fe_lwe_short_int.py
@@ -24,7 +24,7 @@ import numpy as np
 from charm.core.math.integer import randomPrime
 
 from src.helpers.helpers import sample_random_matrix_mod, sample_random_matrix_from_normal_dist, \
-    inner_product_modulo
+    inner_product_modulo, reduce_vector_mod
 from src.helpers.matrix import Matrix
 
 Ciphertext = Dict[str, Matrix]
@@ -32,57 +32,60 @@ Ciphertext = Dict[str, Matrix]
 debug = True
 
 
-def set_up(n: int, vectors_len: int, message_bound: int, vector_bound: int):
-    ip_bound = vectors_len * message_bound * vector_bound  # K in the paper
-    if debug: print("ip bound: ", ip_bound)
-    # ip_bound_bitsize = math.floor(math.log(ip_bound, 2)) + 1
-    # if debug: print("ip bound bitsize: ", ip_bound_bitsize)
-    q = int(randomPrime(1024, 1))
-    if debug: print("q: ", q)
-    m_constraints = n * math.log(q, 2)
-    m = np.random.randint(2 * m_constraints, 4 * m_constraints)
-    if debug: print("m: ", m)
-    alpha = random.random()
-    A = sample_random_matrix_mod((m, n), q)
-    if debug: print("A dims: ", A.size())
-    Z = sample_random_matrix_mod((vectors_len, m), q)
-    if debug: print("Z dims: ", Z.size())
-    U = Z.multiply_modulo(A, q)
-    mpk = {'A': A, 'U': U, 'K': ip_bound, 'P': message_bound, 'V': vector_bound, 'q': q, 'alpha': alpha}
-    msk = Z
-    return mpk, msk
+class FullySecureFeLweShortInt:
 
+    def __init__(self, alpha=None, q=None):
+        self.alpha = alpha
+        self.q = q
 
-def get_functional_key(msk: Matrix, y: List[int]) -> Matrix:
-    return Matrix.from_list(y) @ msk
+    def set_up(self, n: int, vectors_len: int, message_bound: int, vector_bound: int):
+        ip_bound = vectors_len * message_bound * vector_bound  # K in the paper
+        if debug: print("ip bound: ", ip_bound)
+        # ip_bound_bitsize = math.floor(math.log(ip_bound, 2)) + 1
+        # if debug: print("ip bound bitsize: ", ip_bound_bitsize)
+        self.q = int(randomPrime(1024, 1))
+        if debug: print("q: ", self.q)
+        m_constraints = n * math.log(self.q, 2)
+        m = np.random.randint(2 * m_constraints, 4 * m_constraints)
+        if debug: print("m: ", m)
+        self.alpha = random.random()
+        A = sample_random_matrix_mod((m, n), self.q)
+        if debug: print("A dims: ", A.size())
+        Z = sample_random_matrix_mod((vectors_len, m), self.q)
+        if debug: print("Z dims: ", Z.size())
+        U = Z.multiply_modulo(A, self.q)
+        mpk = {'A': A, 'U': U, 'K': ip_bound, 'P': message_bound, 'V': vector_bound}
+        msk = Z
+        return mpk, msk
 
+    def get_functional_key(self, msk: Matrix, y: List[int]) -> Matrix:
+        y = reduce_vector_mod(y, self.q)
+        return Matrix.from_list(y) @ msk
 
-def encrypt(mpk: dict, x: List[int]) -> Ciphertext:
-    A = mpk['A']
-    U = mpk['U']
-    K = mpk['K']
-    m, n = A.size()
-    l = U.size()[0]
-    alpha = mpk['alpha']
-    q = mpk['q']
-    s = sample_random_matrix_mod((1, n), q)
-    x = Matrix.from_list(x)
-    err0 = sample_random_matrix_from_normal_dist((1, m), alpha * q)
-    err1 = sample_random_matrix_from_normal_dist((1, l), alpha * q)
-    if debug: print("U dims: ", U.size())
-    c0 = ((A.multiply_modulo(s.transpose(), q) + err0.transpose()) % q).transpose()
-    c1 = ((U.multiply_modulo(s.transpose(), q) + err1.transpose() + math.floor(q / K) * x.transpose()) % q).transpose()
-    return {'c0': c0, 'c1': c1}
+    def encrypt(self, mpk: dict, x: List[int]) -> Ciphertext:
+        A = mpk['A']
+        U = mpk['U']
+        K = mpk['K']
+        m, n = A.size()
+        l = U.size()[0]
+        s = sample_random_matrix_mod((1, n), self.q)
+        x = Matrix.from_list(x)
+        err0 = sample_random_matrix_from_normal_dist((1, m), self.alpha * self.q)
+        err1 = sample_random_matrix_from_normal_dist((1, l), self.alpha * self.q)
+        if debug: print("U dims: ", U.size())
+        c0 = ((A.multiply_modulo(s.transpose(), self.q) + err0.transpose()) % self.q).transpose()
+        c1 = ((U.multiply_modulo(s.transpose(), self.q) + err1.transpose() + math.floor(
+            self.q / K) * x.transpose()) % self.q).transpose()
+        return {'c0': c0, 'c1': c1}
 
-
-def decrypt(mpk: dict, y: List[int], func_key: Matrix, ciphertext: Ciphertext) -> int:
-    q = mpk['q']
-    ip_bound = mpk['K']
-    c0 = ciphertext['c0'].to_list()[0]
-    c1 = ciphertext['c1'].to_list()[0]
-    ip_approx = (inner_product_modulo(y, c1, q) - inner_product_modulo(func_key.to_list()[0], c0, q)) % q
-    ip_possible_values = [k for k in range(-1 * ip_bound+1, ip_bound, 1)]
-    f = ([abs((q/ip_bound) * k - ip_approx) for k in ip_possible_values])
-    min_index = min(range(len(f)), key=f.__getitem__)
-    ip = ip_possible_values[min_index]
-    return ip
+    def decrypt(self, mpk: dict, y: List[int], func_key: Matrix, ciphertext: Ciphertext) -> int:
+        ip_bound = mpk['K']
+        c0 = ciphertext['c0'].to_list()[0]
+        c1 = ciphertext['c1'].to_list()[0]
+        ip_approx = (inner_product_modulo(y, c1, self.q) - inner_product_modulo(func_key.to_list()[0], c0, self.q)) \
+                    % self.q
+        ip_possible_values = [k for k in range(-1 * ip_bound + 1, ip_bound, 1)]
+        f = ([abs((self.q / ip_bound) * k - ip_approx) for k in ip_possible_values])
+        min_index = min(range(len(f)), key=f.__getitem__)
+        ip = ip_possible_values[min_index]
+        return ip
