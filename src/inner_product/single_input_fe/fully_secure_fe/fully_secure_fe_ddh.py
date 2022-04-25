78a587d177b59bdf51f2a810d2446f2c64e8374b
Cecylia Borek <cecylia.borek@gmail.com>
2022-04-25 14:27:14 +0200

    add docstrings

--- src/inner_product/single_input_fe/fully_secure_fe/fully_secure_fe_ddh.py
+++ src/inner_product/single_input_fe/fully_secure_fe/fully_secure_fe_ddh.py
@@ -25,15 +25,42 @@ import numpy as np
 class FullySecureFE:
 
     def __init__(self, group=None, gen1=None, gen2=None, p=None):
+        """
+        Initializes an instance of Fully Secure Inner Product FE scheme.
+        If we want to make this instance compatible with some other (e.g. to be able to decrypt messages encrypted by
+        the other instance) we should provide the public parameters of the other instance. The set_up method will
+        overwrite the provided parameters
+        Args:
+            group: public group of the other instance
+            gen1: random generator of the group
+            gen2: second random generator of the group
+            p: order of the group
+        """
         self.group = group
         self.gen1 = gen1
         self.gen2 = gen2
         self.p = p
 
     def get_public_params(self):
+        """
+        Returns parameters allowing for initialization of compatible instance of a scheme elsewhere (e.g. to be able to
+        decrypt messages encrypted by the other instance)
+        Returns:
+            dict of parameters of the scheme
+        """
         return {'group': self.group, 'gen1': self.gen1, 'gen2': self.gen2, 'p': self.p}
 
     def set_up(self, security_param: int, vector_length: int) -> (dict, dict):
+        """
+        Sets up parameters needed for proper functioning of the scheme. After calling, the get_public_params function
+        may be called to obtain parameters needed for initializing a compatible instance of the scheme.
+        Args:
+            security_param: security parameter
+            vector_length: supported length of integer vectors
+
+        Returns:
+            (dict, dict): master public key and master secret key
+        """
         self.group = generate_group(security_param)
         self.gen1 = get_random_generator(self.group)
         self.gen2 = get_random_generator(self.group)
@@ -45,11 +72,29 @@ class FullySecureFE:
         return mpk, msk
 
     def get_functional_key(self, msk: dict, y: List[int]) -> dict:
+        """
+        Generates a functional key for integer vector y
+        Args:
+            msk: master secret key
+            y: integer vector y for which the functional key will be generated
+
+        Returns:
+            dict: functional key corresponding to vector y
+        """
         y = reduce_vector_mod(y, self.p)
         func_key = {'s_y': inner_product_group_vector(msk['s'], y), 't_y': inner_product_group_vector(msk['t'], y)}
         return func_key
 
     def encrypt(self, mpk: dict, x: List[int]) -> dict:
+        """
+        Encrypts integer vector x
+        Args:
+            mpk: master public key
+            x: integer vector to be encrypted
+
+        Returns:
+            dict: ciphertext corresponding to vector x
+        """
         ciphertext = {}
         x = reduce_vector_mod(x, self.p)
         r = self.group.random()
@@ -59,6 +104,18 @@ class FullySecureFE:
         return ciphertext
 
     def decrypt(self, func_key: dict, ciphertext: dict, y: List[int], limit: int) -> int:
+        """
+        Recovers the inner product of vectors x and y from x's ciphertext and functional key for y
+        Args:
+            func_key: functional key for vector y
+            ciphertext: ciphertext encrypting vector x
+            y: vector y
+            limit: An upper limit up to which the inner product should be searched for
+
+        Returns:
+            int: the inner product of x and y if it was found within the limit, otherwise None
+
+        """
         e = ciphertext['e']
         intermediate = np.prod([e[i] ** y[i] for i in range(len(y))]) / (
                 ciphertext['c'] ** func_key['s_y'] * ciphertext['d'] ** func_key['t_y']
--- src/inner_product/single_input_fe/fully_secure_fe/fully_secure_fe_lwe_short_int.py
+++ src/inner_product/single_input_fe/fully_secure_fe/fully_secure_fe_lwe_short_int.py
@@ -35,10 +35,41 @@ debug = True
 class FullySecureFeLweShortInt:
 
     def __init__(self, alpha=None, q=None):
+        """
+        Initializes an instance of Fully Secure Inner Product FE scheme for short integers. If we want to make this
+        instance compatible with some other (e.g. to be able to decrypt messages encrypted by the other instance) we
+        should provide parameters alpha and q obtained from the other instance. The set_up method will overwrite the
+        provided parameters
+        Args:
+            alpha: random real value in (0, 1) from the other instance of the scheme
+            q: random prime from the other instance of the scheme
+        """
         self.alpha = alpha
         self.q = q
 
+    def get_params(self):
+        """
+        Returns parameters allowing for initialization of compatible instance of a scheme elsewhere (e.g. to be able to
+        decrypt messages encrypted by the other instance)
+        Returns:
+            dict of parameters of the scheme
+        """
+        return {'alpha': self.alpha, 'q': self.q}
+
     def set_up(self, n: int, vectors_len: int, message_bound: int, vector_bound: int):
+        """
+        Sets ups parameters needed for proper functioning of the scheme. After calling, the get_params function may be
+        called to obtain parameters needed for initializing a compatible instance of the scheme.
+        Args:
+            n:
+            vectors_len: length of the vectors to encrypt and get functional key for
+            message_bound: upper bound for integer components of vectors to encrypt
+            vector_bound: upper bound for integer components of vectors to get functional key for
+
+        Returns:
+            tuple of master public key and master secret key
+
+        """
         ip_bound = vectors_len * message_bound * vector_bound  # K in the paper
         if debug: print("ip bound: ", ip_bound)
         # ip_bound_bitsize = math.floor(math.log(ip_bound, 2)) + 1
@@ -59,10 +90,29 @@ class FullySecureFeLweShortInt:
         return mpk, msk
 
     def get_functional_key(self, msk: Matrix, y: List[int]) -> Matrix:
+        """
+        Generates a functional key for integer vector y
+        Args:
+            msk: the master secret key
+            y: vector of integer components to get functional key for
+
+        Returns:
+            the functional key corresponding to vector y
+        """
         y = reduce_vector_mod(y, self.q)
         return Matrix.from_list(y) @ msk
 
     def encrypt(self, mpk: dict, x: List[int]) -> Ciphertext:
+        """
+        Encrypts integer vector x
+        Args:
+            mpk: master public key
+            x: vector of integers to encrypt
+
+        Returns:
+            the ciphertext encrypting vector x
+
+        """
         A = mpk['A']
         U = mpk['U']
         K = mpk['K']
@@ -79,6 +129,17 @@ class FullySecureFeLweShortInt:
         return {'c0': c0, 'c1': c1}
 
     def decrypt(self, mpk: dict, y: List[int], func_key: Matrix, ciphertext: Ciphertext) -> int:
+        """
+        Recovers the inner product of vectors x and y from x's ciphertext and y's functional key
+        Args:
+            mpk: the master public key
+            y: vector for which the func key was calculated
+            func_key: the functional key for vector y
+            ciphertext: ciphertext encrypting vector x
+
+        Returns:
+            the inner product of vectors x and y
+        """
         ip_bound = mpk['K']
         c0 = ciphertext['c0'].to_list()[0]
         c1 = ciphertext['c1'].to_list()[0]
